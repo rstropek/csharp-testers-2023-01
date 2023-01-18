3e890152d874b4dee2dffa3f1de6e7ce3dd069e8
Rainer Stropek <rainer@timecockpit.com>
2023-01-18 13:36:11 +0100

    Add pipeline

--- TicTacToe/TicTacToe/TicTacToe.Logic/Game.cs
+++ TicTacToe/TicTacToe/TicTacToe.Logic/Game.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToe.Logic
+﻿namespace TicTacToe.Logic
 {
     public class Game
     {
